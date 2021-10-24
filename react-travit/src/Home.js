b8cbb2199a601d973386468249f34f62a4e3feba
Emmanuel289 <emmirald@gmail.com>
2021-10-23 21:22:49 -0600

    Added static files and test file for home component and updated the landing page

---
+++ react-travit/src/Home.js
@@ -0,0 +1,17 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1 class="display-block text-center">
+        <strong>Welcome to Travit</strong>
+        <p>
+          Travit is your ultimate travel guide to booking trips to top
+          destinations around the world
+        </p>
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
