99cc0cf13419b3d7fd77bd8d74b86b11e8addc03
Emmanuel289 <emmirald@gmail.com>
2023-01-26 01:36:28 -0700

    Component and styling for sliding background images

---
+++ travit/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './navbar.css';
+
+
+function NavBar() {
+
+    return (
+    <div className="toolbar" role="banner">
+    </div>
+    );
+}
+
+export { NavBar }; 
