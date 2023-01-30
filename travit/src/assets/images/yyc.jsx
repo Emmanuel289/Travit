e0e74253abc3553e14e2edcb936bd33b5d7739d6
Emmanuel289 <emmirald@gmail.com>
2023-01-29 23:22:22 -0700

    Fix images path

---
+++ travit/src/assets/images/yyc.jsx
@@ -0,0 +1,72 @@
+export const images = [
+    <img className="beltline-aerial"
+          src="https://travit-images.s3.ca-central-1.amazonaws.com/images/beltline1.JPG"
+          alt="beltine-aerial"
+          style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+          }}
+        />,
+    <img className="beltline-2"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/beltline3.JPG"
+        alt="beltline-street"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+          }}
+      />,
+    <img className="yyc-eleven"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/yyc_eleven.JPG"
+        alt="beltine-tallest-tower"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+        }}
+    />,
+    <img className="yyc-se-1"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/yyc_se1.JPG"
+        alt="yyc-se-1"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+        }}
+        />,
+    <img className="yyc-se-2"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/yyc_se2.JPG"
+        alt="yyc-se-2"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+        }}
+        />,
+    <img className="yyc-heritage-park-east"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/yyc_heritage_park1.JPG"
+        alt="yyc-heritage-park-east"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+        }}
+        />,
+    <img className="yyc-heritage-park-west"
+        src="https://travit-images.s3.ca-central-1.amazonaws.com/images/yyc_heritage_park2.JPG"
+        alt="yyc-heritage-park-west"
+        style={{
+            position: 'relative',
+            width: '400px',
+            height: '400px',
+            display: 'inline-block',
+          }}
+        />,
+]
