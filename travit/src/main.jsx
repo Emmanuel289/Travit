13fa849298dbf02a144091c2aac2a57a3d682fb1
Emmanuel289 <emmirald@gmail.com>
2023-02-15 05:31:46 -0700

    content for use-cases

--- travit/src/main.jsx
+++ travit/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Footer } from './components/footer/footer'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
