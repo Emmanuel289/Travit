73a04d078a0c03b3f86885703773b51d8c664185
Emmanuel289 <emmirald@gmail.com>
2023-02-15 12:42:49 -0700

    redacting footer and updating checkmarks

--- travit/src/main.jsx
+++ travit/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Footer } from './components/footer/footer'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Footer/> 
   </React.StrictMode>,
 )
