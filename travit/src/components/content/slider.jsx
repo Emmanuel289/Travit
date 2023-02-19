d3ea5da600b4074b6505b34a6cecc3e058de953d
Emmanuel289 <emmirald@gmail.com>
2023-02-18 17:39:23 -0700

    Directory reorg

---
+++ travit/src/components/content/slider.jsx
@@ -0,0 +1,27 @@
+import { React, useState} from 'react';
+import './slider.css';
+
+
+const Slider = ({ images }) => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const loadImage = (index) => {
+    setCurrentIndex(index);  
+  };
+  
+  return (
+    <div>
+        <div className='image-slider'>{images[currentIndex]}</div>
+         <div className="owl-dots" >
+          {images.map((image, index) => {
+            return (
+              <button key={index} role="button" className="owl-dot" tabIndex="0" onClick={() => loadImage(index)}><span></span></button>
+            )
+          })}
+        </div>
+    </div>
+  )
+};
+
+export { Slider };
