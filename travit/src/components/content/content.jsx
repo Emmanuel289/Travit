11b064372d6ddbdd23c2c63fbf4ee48e951c92cf
Emmanuel289 <emmirald@gmail.com>
2023-02-24 11:43:10 -0700

    Fix

--- travit/src/components/content/content.jsx
+++ travit/src/components/content/content.jsx
@@ -7,10 +7,10 @@ function Content() {
     <h1>Welcome to Travit</h1>
     <section>
       <div>Travit is your all-in-one solution for planning your trips.</div>
-      <div><Checkbox checked style={{marginBottom: '10px', marginTop: '10px'}} label='Travel suggestions at your finger-tips based on your unique preferences, budget, and travel history.' /></div>
-      <div><Checkbox checked style={{marginBottom: '10px'}} label='Save time on researching destinations and get all the information you need in one place.'/></div>
-      <div><Checkbox checked style={{marginBottom: '10px'}} label='Find the best deals on flights, hotels, and other leisure activities in your location'/></div>
-      <div><Checkbox checked style={{marginBottom: '10px'}} label='Reliable and trustworthy reviews and ratings from the community of Travit users, just like you.'/></div>
+      <div><Checkbox checked style={{ marginBottom: '10px', marginTop: '10px' }} label='Travel suggestions at your finger-tips based on your unique preferences, budget, and travel history.' /></div>
+      <div><Checkbox checked style={{ marginBottom: '10px' }} label='Save time on researching destinations and get all the information you need in one place.'/></div>
+      <div><Checkbox checked style={{ marginBottom: '10px' }} label='Find the best deals on flights, hotels, and other leisure activities in your location.'/></div>
+      <div><Checkbox checked style={{ marginBottom: '10px' }} label='Reliable and trustworthy reviews and ratings from the community of Travit users, just like you.'/></div>
     </section>
   </div>
   );
