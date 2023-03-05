d661dccd5a8038b9bd1082d4f46e7d1c4046ed91
Emmanuel289 <emmirald@gmail.com>
2023-03-05 01:14:05 -0700

    changing grid display to flex

--- travit/src/components/content/content.jsx
+++ travit/src/components/content/content.jsx
@@ -6,22 +6,18 @@ function Content() {
       <h2>Travit Enriches Your Travel Experience</h2>
     <div className="wrapper">
       <div className="grid bg1">
-        <h2>Create Your Profile</h2>
-        <span><p>Setup your profile with an email and password</p></span>
-      </div>
-      <div className="grid bg2">
         <h2>Travel Recommendations</h2>
         <span><p>Travel suggestions tailored to your unique preferences and budget</p></span>
       </div>
-      <div className="grid bg3">
-        <h2>Relevant Information For Your Preferred Destinations</h2>
+      <div className="grid bg2">
+        <h2>Relevant Information</h2>
         <span><p>Save time on researching destinations and get all the information you need in one place</p></span>
       </div>
-      <div className="grid bg4">
+      <div className="grid bg3">
         <h2>Get the Best Deals</h2>
         <span><p>Find the best deals on flights, hotels, and other leisure activities in your location.</p></span>
       </div>
-      <div className="grid bg5">
+      <div className="grid bg4">
         <h2>Engage With The Community</h2>
         <span><p>Reliable reviews and ratings from the community of Travit users</p></span>
       </div>
