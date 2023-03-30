e5e92befe26197d4a8892e70f6811e7011e77e78
Emmanuel289 <emmirald@gmail.com>
2023-03-30 00:24:08 -0600

    removed setup hook for user and tests for destination

--- travit_service/destinations/models.py
+++ travit_service/destinations/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Destination(models.Model):
     """Represents a destination for a user to travel to."""
     name = models.CharField(verbose_name="Name", blank=False, max_length=50)
     description = models.TextField(verbose_name="Description", blank=False)
-    image = models.ImageField(verbose_name="Image", blank=False, upload_to="images/")
-    weather = models.CharField(verbose_name="Weather", blank=False, max_length=50)
-    currency = models.CharField(verbose_name="Currency", blank=False, max_length=50)
-    timezone = models.CharField(verbose_name="Timezone", blank=False, max_length=50, default=timezone.get_current_timezone_name())
+    image = models.ImageField(verbose_name="Image", upload_to="images/")
+    weather = models.CharField(verbose_name="Weather", max_length=50)
+    currency = models.CharField(verbose_name="Currency", max_length=50)
+    timezone = models.CharField(
+        verbose_name="Timezone", max_length=50, default=timezone.get_current_timezone_name())
+
+    def __str__(self):
+        return f'Destination(id={self.id}, name={self.name})'
--- travit_service/destinations/tests.py
+++ travit_service/destinations/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.forms import ValidationError
+from destinations.models import Destination
 
-# Create your tests here.
+
+class DestinationTestCase(TestCase):
+
+    def test_valid_destination(self):
+        Destination.objects.create(name="test destination", description="test description",
+                                   image="test image.jpg", weather="test weather", currency="test currency", timezone="test timezone")
+        Destination.objects.create(
+            name="second test destination", description="second test description")
+
+        first_destination = Destination.objects.get(name="test destination")
+        second_destination = Destination.objects.get(
+            name="second test destination")
+
+        self.assertEqual(len(Destination.objects.all()), 2)
+        self.assertEqual(Destination.objects.first(), first_destination)
+        self.assertEqual(Destination.objects.get_queryset()
+                         [1], second_destination)
+
+        self.assertEqual(first_destination.name, "test destination")
+        self.assertEqual(first_destination.description, "test description")
+        self.assertEqual(first_destination.image, "test image.jpg")
+        self.assertEqual(first_destination.weather, "test weather")
+        self.assertEqual(first_destination.currency, "test currency")
+        self.assertEqual(first_destination.timezone, "test timezone")
+
+    def test_invalid_destination(self):
+        destination = Destination.objects.create(
+            name="test destination")
+
+        with self.assertRaises(ValidationError):
+            destination.full_clean()
