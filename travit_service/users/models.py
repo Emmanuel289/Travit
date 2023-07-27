8c347bb5465907cddcfb60828c06968a5cfe1ad9
Emmanuel289 <emmirald@gmail.com>
2023-07-27 07:44:52 -0600

    Enabling cors for all and user auth with email

--- travit_service/users/models.py
+++ travit_service/users/models.py
@@ -3,14 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(
-        verbose_name="Username", blank=False, max_length=255, unique=True
-    )
-    email = models.EmailField(verbose_name="Email", blank=False, unique=True)
-    password = models.CharField(verbose_name="Password", blank=False, max_length=255)
+    first_name = models.CharField(
+        verbose_name='first_name', blank=False, max_length=150)
+    last_name = models.CharField(
+        verbose_name='last_name', blank=False, max_length=150)
+    email = models.EmailField(
+        verbose_name="email_address", blank=False, unique=True)
+    re_password = models.CharField(
+        verbose_name="Retype Password", blank=False, max_length=255, default='')
 
     class Meta:
         ordering = ["id"]
 
     def __str__(self):
-        return f"User(id={self.id}, email={self.email})"
+        return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, email={self.email})"
