a58dda198452c3994eac63a397e0da9360a5fe15
Emmanuel289 <emmirald@gmail.com>
2023-07-27 07:46:12 -0600

    Adding user serializer, view, and urlpattern

---
+++ travit_service/users/serializers.py
@@ -0,0 +1,10 @@
+# from rest_framework import serializers
+from djoser.serializers import UserCreatePasswordRetypeSerializer as BaseUserCreatePasswordRetypeSerializer
+
+from .models import User
+
+
+class UserCreatePasswordRetypeSerializer(BaseUserCreatePasswordRetypeSerializer):
+    class Meta(BaseUserCreatePasswordRetypeSerializer.Meta):
+        fields = ["id", "first_name", "last_name", "username",
+                  "email", "password", "re_password"]
