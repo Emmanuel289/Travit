eed78fec35d48abf08a595361ea700ce8fac943d
Emmanuel289 <emmirald@gmail.com>
2025-11-17 13:13:23 -0500

    chore

--- service/scripts/run_tests.sh
+++ service/scripts/run_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Setup the Python virtual environment
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/setup_env.sh"
 
 # Verify that environment variables have been set correctly
