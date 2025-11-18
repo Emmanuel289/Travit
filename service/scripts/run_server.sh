b6e49c84afc74da8a32b70daea8c83d6a83e908b
Emmanuel289 <emmirald@gmail.com>
2025-11-17 23:52:39 -0500

    fix errors

--- service/scripts/run_server.sh
+++ service/scripts/run_server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -eu -o pipefail
 
 # Setup the Python virtual environment
--- service/scripts/setup_env.sh
+++ service/scripts/setup_env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -eu -o pipefail
 
 function check_not_empty {
