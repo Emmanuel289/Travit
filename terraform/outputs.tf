a11bbd1fb9590cdb4a8cbedb6832ca4c3b547e3f
Okechukwu  Emmanuel Ochia <emmirald@gmail.com>
2025-11-20 15:24:15 -0500

    Migrate AWS resources to GCP (#149)
    * migrate AWS resources to GCP
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,4 @@
-output "svc_hostname" {
-  value       = aws_instance.travit_svc_instance.public_dns
-  description = "Hostname assigned to the instance"
-}
-
-output "username" {
-  value       = local.username
-  description = "Username used to connect to the EC2 instance"
-}
-output "remote_cache" {
-  value       = var.remote_cache
-  description = "Location of cache data and state files on the EC2 instance"
-}
-
-output "db_hostname" {
-  value       = aws_db_instance.travit_db_instance.address
-  description = "DB hostname"
-}
-
-output "db_name" {
-  value       = aws_db_instance.travit_db_instance.db_name
-  description = "DB name"
+output "project_id" {
+  value       = local.project
+  description = "The GCP project"
 }
